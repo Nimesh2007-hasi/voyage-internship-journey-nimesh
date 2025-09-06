d0bb645b64572bae4b88fb4f1a9960cafbd6c032
Nimesh Hasith <nimeshhasith2007@gmail.com>
2025-09-06 23:56:12 +0530

    Initial commit: React app setup

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+function App() {
+ const age=18;
+ const country="Sri Lanka";
+const getFullName=(f,m,l) =>{
+  return `${f} ${m} ${l}`;
+}
+const arr= ["BMW","Porche","Ferrari"];
+  return (
+    <>
+      <h1 className='heading'>My First App with React</h1>
+      <h1>Employee Details</h1>
+      <p>Full Name: {getFullName("WG","Nimesh","Hasith")}</p>
+      <p>Age :{age} </p>
+      <p>Country :{country}</p>
+      <p>Nimesh likes {arr[0]}</p>
+      <p>Languages</p>
+      <ul>
+        <li>Html</li>
+        <li>Java</li>
+        <li>Python</li>
+
+      </ul>
+    </>
+  )
+}
+
+export default App
